78c7e31a840bf366b838f5694cf6e0ab049c2832
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-06 19:19:07 +0900

    [add] 회원이 등록한 상품의 정보를 알려주는 로직 구현
    EndPoint ex) .../product/find?userid=2

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductCreateDto } from './productDto/product.create.dto';
+import { ProductFindDto } from './productDto/product.find.dto';
 
 @Controller('product')
 export class ProductController {
@@ -9,5 +10,9 @@ export class ProductController {
   @Get('create')
   async create(@Query() query: ProductCreateDto) {
     return this.productService.create(query);
+  }
+  @Get('find')
+  async find(@Query() query: ProductFindDto) {
+    return this.productService.find(query);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from '../Entity/product.entity';
 import { Repository } from 'typeorm';
 import { ProductCreateDto } from './productDto/product.create.dto';
 import { UserEntity } from '../Entity/user.entity';
+import { ProductFindDto } from './productDto/product.find.dto';
 
 @Injectable()
 export class ProductService {
@@ -21,5 +22,28 @@ export class ProductService {
 
     await this.productEntity.save(product);
     return '상품등록이 되었습니다!';
+  }
+  async find(query: ProductFindDto) {
+    const data: ProductEntity[] = await this.productEntity.find({
+      relations: {
+        user: true,
+      },
+      where: {
+        user: {
+          id: query.userid,
+        },
+      },
+      select: {
+        user: {
+          id: false,
+          userName: true,
+          userID: false,
+          userPW: false,
+        },
+      },
+    });
+    console.log(data);
+    if (!data[0]) throw new NotFoundException('등록되어 있지 않습니다.');
+    return data;
   }
 }
