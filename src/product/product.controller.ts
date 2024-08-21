fc54c2f07308ca96aea5d1fc63daefc187a2e73c
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-21 16:23:01 +0900

    [edit] product controller 에 대한 Responses 수정

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -13,7 +13,7 @@ import { ProductFindDto } from './productDto/product.find.dto';
 import { ProductEditDto } from './productDto/product.edit.dto';
 import { ProductDeleteDto } from './productDto/product.delete.dto';
 import { AccessGuard } from '../jwt/access.guard';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('product')
 @ApiTags('Product Api')
@@ -25,6 +25,11 @@ export class ProductController {
     summary: '상품 등록 API',
     description: '해당 사용자의 id로 상품이 등록된다.',
   })
+  @ApiResponse({
+    status: 201,
+    description: '상품등록이 되었습니다!',
+  })
+  @ApiBearerAuth()
   async create(@Query() query: ProductCreateDto, @Request() req: Request) {
     return this.productService.create(query, req);
   }
@@ -33,6 +38,15 @@ export class ProductController {
     summary: '상품 정보 출력 API',
     description: '해당 사용자가 어떤 상품을 등록했는지 알 수 있는 Api이다.',
   })
+  @ApiResponse({
+    status: 201,
+    description: '상품등록에 대한 data 출력.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: '등록되어 있지 않습니다.',
+  })
+  @ApiBearerAuth()
   async find(@Query() query: ProductFindDto, @Request() req: Request) {
     return this.productService.find(query, req);
   }
@@ -41,6 +55,15 @@ export class ProductController {
     summary: '상품 정보 수정 API',
     description: '상품에 대한 정보를 수정한다.',
   })
+  @ApiResponse({
+    status: 201,
+    description: '변경된 상품에 대한 정보 출력.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: '등록되어있지 않은 상품입니다.',
+  })
+  @ApiBearerAuth()
   async edit(@Query() query: ProductEditDto) {
     return this.productService.edit(query);
   }
@@ -49,6 +72,15 @@ export class ProductController {
     summary: '상품 정보 삭제 API',
     description: '상품에 대한 정보를 삭제.',
   })
+  @ApiResponse({
+    status: 201,
+    description: '상품이 삭제되었습니다.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: '등록되어있지 않는 상품 입니다.',
+  })
+  @ApiBearerAuth('access-token')
   async delete(@Query() query: ProductDeleteDto) {
     return this.productService.delete(query);
   }
