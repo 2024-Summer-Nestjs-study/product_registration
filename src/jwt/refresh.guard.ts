b67378da078b638ab712d304b74086f90b9bb47c
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-27 17:15:34 +0900

    [edit] refreshKey env를 사용하여 변경

--- src/jwt/refresh.guard.ts
+++ src/jwt/refresh.guard.ts
@@ -5,10 +5,14 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RefreshGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
@@ -17,7 +21,7 @@ export class RefreshGuard implements CanActivate {
       console.log('헤드가 비어있습니다.');
       throw new UnauthorizedException();
     }
-    const secretR = '1234';
+    const secretR = this.configService.get('refresh_Key');
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: secretR,
@@ -26,10 +30,12 @@ export class RefreshGuard implements CanActivate {
     } catch (e) {
       if (e.name === 'JsonWebTokenError') {
         throw new UnauthorizedException('refresh 아닌데?');
-      } else if (e.name === 'TokenExpiredError') {
+      }
+      //Check here
+      if (e.name === 'TokenExpiredError') {
         throw new UnauthorizedException('만료되었습니다. 다시 로그인 하세요.');
       }
-      console.log(e);
+      console.log(e.name);
       throw new UnauthorizedException();
     }
     return true;
