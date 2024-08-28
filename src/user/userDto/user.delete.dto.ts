dbdab5a024892b7af61ad1de888528ab4896bdd2
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-28 15:15:37 +0900

    [edit] user 회원탈퇴 dto 수정

--- src/user/userDto/user.delete.dto.ts
+++ src/user/userDto/user.delete.dto.ts
@@ -2,12 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UserDeleteDto {
-  @ApiProperty({ description: '이름' })
-  @IsString()
-  userName: string;
-  @ApiProperty({ description: '아이디' })
-  @IsString()
-  userID: string;
   @ApiProperty({ description: '비밀번호' })
   @IsString()
   userPW: string;
