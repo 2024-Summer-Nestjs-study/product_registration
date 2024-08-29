eade2c217d94f04e2ae542f6cf89aca0de9ab94e
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-29 09:30:31 +0900

    [edit] 상품 등록에 관한 dto 수정

--- src/product/productDto/product.create.dto.ts
+++ src/product/productDto/product.create.dto.ts
@@ -1,11 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumberString, IsString } from 'class-validator';
 
 export class ProductCreateDto {
   @ApiProperty({ description: '상품 이름' })
   @IsString()
   name: string;
   @ApiProperty({ description: '상품 가격' })
-  @IsNumber()
+  @IsNumberString()
   price: number;
 }
