1a7dd6a09e77d9a5b9c3df5d777d46d3455c9387
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-29 23:42:51 +0900

    [delete] 상품을 찾을 때, 로그인 되어있는 회원의 상품이 출력하기 위해 삭제

--- src/product/productDto/product.find.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
-
-export class ProductFindDto {
-  @ApiProperty({ description: '상품을 등록한 userid' })
-  @IsNumber()
-  userid: number;
-}
