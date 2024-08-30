59e331bdfd809ce7e0ab1804211c047efee8c371
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-30 20:46:20 +0900

    [edit] 상품명이 겹치지 않도록 @Unique를 사용하여 entity 수정

--- src/Entity/product.entity.ts
+++ src/Entity/product.entity.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm';
 import { DefaultEntity } from './default.entity';
 import { UserEntity } from './user.entity';
 
 @Entity()
+@Unique(['name'])
 export class ProductEntity extends DefaultEntity {
   @ManyToOne((type) => UserEntity, (user: UserEntity) => user.id)
   @JoinColumn()
