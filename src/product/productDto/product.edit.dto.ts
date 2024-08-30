7466688d3f6f2b494b4e7f0430f580e7bd899d5c
oxoxoxoxox <dudwlsaos132@naver.com>
2024-08-30 20:46:46 +0900

    [edit] 상품 수정 dto - price Validation수정

--- src/product/productDto/product.edit.dto.ts
+++ src/product/productDto/product.edit.dto.ts
@@ -1,11 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
-
+import { IsNumberString, IsString } from 'class-validator';
 export class ProductEditDto {
   @ApiProperty({ description: '상품 이름' })
   @IsString()
   name: string;
   @ApiProperty({ description: '상품 가격' })
-  @IsNumber()
+  @IsNumberString()
   price: number;
 }
