0156f16bd8ef3b1ed8af79386471ef007ea99424
oxoxoxoxox <dudwlsaos132@naver.com>
2024-09-02 12:56:54 +0900

    [delete] 상품 이름에 대한 Unique 설정 삭제

--- src/Entity/product.entity.ts
+++ src/Entity/product.entity.ts
@@ -1,9 +1,8 @@
-import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { DefaultEntity } from './default.entity';
 import { UserEntity } from './user.entity';
 
 @Entity()
-@Unique(['name'])
 export class ProductEntity extends DefaultEntity {
   @ManyToOne((type) => UserEntity, (user: UserEntity) => user.id)
   @JoinColumn()
